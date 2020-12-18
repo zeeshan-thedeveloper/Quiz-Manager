fde366d2adbeb60645d2dfa129867be5d98f0143
Sayed Rizwan Ahmed Shah <bukharir322@gmail.com>
2020-12-18 17:23:14 +0500

    Version #1

--- SignInSignUpModule/Rizwan/CreatingSingInSignUpForm/CreatingSingInSignUpForm/Form1.Designer.cs
+++ SignInSignUpModule/Rizwan/CreatingSingInSignUpForm/CreatingSingInSignUpForm/Form1.Designer.cs
@@ -134,7 +134,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(64, 301);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 15);
@@ -145,7 +145,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.ForeColor = System.Drawing.Color.Red;
             this.label3.Location = new System.Drawing.Point(67, 214);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 15);
