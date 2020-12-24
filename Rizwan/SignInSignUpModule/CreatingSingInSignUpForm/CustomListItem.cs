5016643af1ea5148801254a6c697b8de4a7c94f3
Sayed Rizwan Ahmed Shah <bukharir322@gmail.com>
2020-12-25 00:24:04 +0500

    Just added customlist component

---
+++ Rizwan/SignInSignUpModule/CreatingSingInSignUpForm/CustomListItem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomList
+{
+    public partial class CustomListItem : UserControl
+    {
+        public CustomListItem()
+        {
+            InitializeComponent();
+        }
+        #region  
+        private string _question;
+        private string _options;
+        private Image _icon;
+        private Color _iconbackground;
+        
+        [Category("Custom Prop")]
+        public string Question
+        {
+            get { return _question; }
+            set { _question = value;labelQuestion.Text = value; }
+        }
+
+        [Category("Custom Prop")]          
+        public string Options
+        {
+            get { return _options; }
+            set { _options = value;richTextBoxOptions.Text = value; }
+        }
+
+
+        [Category("Custom Prop")]
+        public Image Icon
+        {
+            get { return _icon; }
+            set { _icon = value; pictureBox1.Image = value; }
+        }
+        
+        [Category("Custom Prop")]
+        public Color IconBackground
+        {
+            get { return _iconbackground; }
+            set { _iconbackground = value; panel1.BackColor = value; }
+        }
+
+
+        #endregion
+
+        private void CustomListItem_MouseHover(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Silver;
+        }
+
+        private void CustomListItem_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(128, 128, 255);
+        }
+
+        private void CustomListItem_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Title is : "+labelQuestion.Text+ "\n Description is : " +richTextBoxOptions.Text);
+
+        }
+    }
+}
---
+++ Rizwan/SignInSignUpModule/CreatingSingInSignUpForm/CustomListItem.designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CustomList
+{
+    partial class CustomListItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBoxOptions = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(137, 117);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuestion.Location = new System.Drawing.Point(139, 0);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(263, 51);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "Question";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(577, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBoxOptions
+            // 
+            this.richTextBoxOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.richTextBoxOptions.Location = new System.Drawing.Point(143, 47);
+            this.richTextBoxOptions.Name = "richTextBoxOptions";
+            this.richTextBoxOptions.Size = new System.Drawing.Size(367, 61);
+            this.richTextBoxOptions.TabIndex = 5;
+            this.richTextBoxOptions.Text = "Here will be available options";
+            // 
+            // CustomListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.richTextBoxOptions);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.panel1);
+            this.Name = "CustomListItem";
+            this.Size = new System.Drawing.Size(698, 127);
+            this.Load += new System.EventHandler(this.CustomListItem_Load);
+            this.MouseLeave += new System.EventHandler(this.CustomListItem_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.CustomListItem_MouseHover);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBoxOptions;
+    }
+}
